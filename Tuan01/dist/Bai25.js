f3ab447ece1a55ac11ba5002d74090057b927ebc
daocongdanh <daocongdanh47@gmail.com>
2025-08-24 11:26:22 +0700

    Done Bai25

---
+++ Tuan01/dist/Bai25.js
@@ -0,0 +1,8 @@
+"use strict";
+// 25. Create a class Shape with a static method describe().
+class Shape {
+    static describe() {
+        console.log("Đây là một hình học (Shape).");
+    }
+}
+Shape.describe();
---
+++ Tuan01/src/Bai25.ts
@@ -0,0 +1,7 @@
+// 25. Create a class Shape with a static method describe().
+class Shape {
+  static describe(): void {
+    console.log("Đây là một hình học (Shape).");
+  }
+}
+Shape.describe()
