7b6f84bfcc83487ef9963623ede0c4e069a66ce5
H82M19\Student <Student>
2025-08-30 13:41:52 +0700

    Done Bai24

---
+++ Tuan02/dist/Bai24.js
@@ -0,0 +1,15 @@
+"use strict";
+// 24. Write an async function postData() that sends a POST request to a test API.
+async function postData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: "Hello", body: "World", userId: 1 })
+    });
+    return res.json();
+}
+async function run24() {
+    const result = await postData();
+    console.log(result);
+}
+run24();
