a63789ec085c53962b8e9779d488381844252dd3
H82M19\Student <Student>
2025-08-30 12:56:06 +0700

    Done Bai03

---
+++ Tuan02/src/Bai03.ts
@@ -0,0 +1,11 @@
+// 3. Write a function that rejects a Promise with the error "Something went wrong" after 1 second.
+
+const getError = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject("Something went wrong");
+    }, 1000);
+  });
+}
+
+getError().catch(err => console.error(err));
