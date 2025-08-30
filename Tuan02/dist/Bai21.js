17e5e8b0aa8fb8065c4d7d84fcf98ce7a82bd1e3
H82M19\Student <Student>
2025-08-30 13:33:25 +0700

    Done Bai21

---
+++ Tuan02/dist/Bai21.js
@@ -0,0 +1,8 @@
+"use strict";
+// 21. Use fetch to get data from a public API (https://jsonplaceholder.typicode.com/todos/1)
+async function run21() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await res.json();
+    console.log(data);
+}
+run21();
