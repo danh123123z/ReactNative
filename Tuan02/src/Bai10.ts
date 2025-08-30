cb7481c6ad9dc945e25e17cc916ccde9b2990e2d
H82M19\Student <Student>
2025-08-30 13:12:25 +0700

    Done Bai10

---
+++ Tuan02/src/Bai10.ts
@@ -0,0 +1,15 @@
+// 10. Use .finally() to log "Done" when a Promise finishes (success or failure).
+
+const bai10Promise = new Promise((resolve, reject) => {
+  const num = Math.random();
+  if (num > 0.5) {
+    resolve("Success: " + num);
+  } else {
+    reject("Error: number too small");
+  }
+});
+
+bai10Promise
+  .then(result => console.log(result))
+  .catch(err => console.error(err))
+  .finally(() => console.log("Done"));
