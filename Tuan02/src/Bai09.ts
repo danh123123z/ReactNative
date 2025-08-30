a322b8e79133737ca0d8cb14c995508e2a508ff5
H82M19\Student <Student>
2025-08-30 13:08:15 +0700

    Done Bai09

---
+++ Tuan02/src/Bai09.ts
@@ -0,0 +1,12 @@
+// 9. Write a Promise that reads an array after 1 second and filters even numbers.
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evenPromise = new Promise((resolve) => {
+  setTimeout(() => {
+    const evens = numbers.filter(n => n % 2 === 0);
+    resolve(evens);
+  }, 1000);
+});
+
+evenPromise.then(result => console.log("Số chẳn:", result));
