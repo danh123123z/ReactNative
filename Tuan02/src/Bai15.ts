d9486778a09c4fd280c9fa41df408a30c8c4a7f0
H82M19\Student <Student>
2025-08-30 13:20:33 +0700

    Done Bai15

---
+++ Tuan02/src/Bai15.ts
@@ -0,0 +1,19 @@
+// 15. Call multiple async functions sequentially using await.
+
+async function task15(name: string, time: number): Promise<string> {
+  await new Promise(resolve => setTimeout(resolve, time));
+  return `${name} done`;
+}
+
+async function run15() {
+  const r1 = await task15("Task 1", 1000);
+  console.log(r1);
+
+  const r2 = await task15("Task 2", 1000);
+  console.log(r2);
+
+  const r3 = await task15("Task 3", 1000);
+  console.log(r3);
+}
+
+run15();
