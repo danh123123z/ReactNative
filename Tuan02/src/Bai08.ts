d2aac7341ebd672d4b50ef2806c71beb50a70153
H82M19\Student <Student>
2025-08-30 13:06:34 +0700

    Done Bai08

---
+++ Tuan02/src/Bai08.ts
@@ -0,0 +1,7 @@
+// 8. Create a Promise chain: square the number 2, then double it, then add 5.
+
+Promise.resolve(2)
+  .then(num => num * num) 
+  .then(num => num * 2)   
+  .then(num => num + 5)   
+  .then(result => console.log("Kết quả:", result));
