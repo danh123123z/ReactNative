e813418fa656fe6f853be7d0e5c0cb6321f83906
H82M19\Student <Student>
2025-08-30 13:34:46 +0700

    Done Bai22

---
+++ Tuan02/dist/Bai22.js
@@ -0,0 +1,10 @@
+"use strict";
+// 22. Call the API multiple times and log the results.
+async function run22() {
+    for (let i = 1; i <= 3; i++) {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${i}`);
+        const data = await res.json();
+        console.log(data);
+    }
+}
+run22();
